ffca5e72ba885a43db954f02f1710f3cbf0ff22d
dextch <8195526+the-dext@users.noreply.github.com>
2021-09-12 13:18:21 +0100

    example without ifs

--- eliminating-if-statements/Program.cs
+++ eliminating-if-statements/Program.cs
@@ -4,28 +4,39 @@
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            PrintMenu();
+            // infinite loop to keep the app going until a valid choice is made
+            do
+            {
+                PrintMenu();
 
-            var choice = Console.ReadKey();
-            
-            if (choice.KeyChar == 'D' || choice.KeyChar == 'd')
-            {
-                PrintResult("You voted for Dogs");
-            }
-            else if (choice.KeyChar == 'C' || choice.KeyChar == 'c')
-            {
-                PrintResult("You voted for Cats");
-            }
-            else if (choice.KeyChar == 'R' || choice.KeyChar == 'r')
-            {
-                PrintResult("You voted for Rabbits");
-            }
+                var choice = Console.ReadKey();
+
+                if (choice.KeyChar == 'D' || choice.KeyChar == 'd')
+                {
+                    PrintResult("You voted for Dogs");
+                }
+                else if (choice.KeyChar == 'C' || choice.KeyChar == 'c')
+                {
+                    PrintResult("You voted for Cats");
+                }
+                else if (choice.KeyChar == 'R' || choice.KeyChar == 'r')
+                {
+                    PrintResult("You voted for Rabbits");
+                }
+                else
+                {
+                    // invalid option so try again
+                    Console.WriteLine("Invalid selection, press return and select an option from the menu");
+                    Console.ReadLine();
+                }
+            } while (true);
         }
 
         static void PrintMenu()
         {
+            Console.Clear();
             Console.WriteLine("***********");
             Console.WriteLine("** With If Statements Demo **");
             Console.WriteLine("***********");
---
+++ without-if-statements/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace without_if_statements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        private static List<(Func<char, bool> predicate, Action action)> commandMap;
+        
+        private static void SetupCommandMap ()
+        {
+            commandMap = new List<(Func<char, bool> predicate, Action action)>();
+
+            commandMap.Add(((choice) => choice == 'D' || choice == 'd', () => PrintResult("You voted for Dogs")));
+            commandMap.Add(((choice) => choice == 'C' || choice == 'c', () => PrintResult("You voted for Cats")));
+            commandMap.Add(((choice) => choice == 'R' || choice == 'r', () => PrintResult("You voted for Rabbits")));
+            
+            // a default action to take when no command is matched
+            commandMap.Add(((choice) => true, () => {
+                Console.WriteLine("Invalid selection, press return and select an option from the menu");
+                Console.ReadLine();
+                PrintMenu();
+            }));
+        }
+
+        static void Main()
+        {
+            SetupCommandMap();
+            PrintMenu();
+
+            do
+            {
+                var choice = Console.ReadKey().KeyChar;
+                commandMap.First(x => x.predicate(choice)).action();
+            } while (true);
+        }
+
+        static void PrintMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("***********");
+            Console.WriteLine("** With If Statements Demo **");
+            Console.WriteLine("***********");
+            Console.WriteLine();
+            Console.WriteLine("Vote for your favourite pet");
+            Console.WriteLine();
+
+            Console.WriteLine("D - Dogs");
+            Console.WriteLine("C - Cats");
+            Console.WriteLine("R - Rabbits");
+        }
+
+        static void PrintResult(string msg)
+        {
+            Console.Clear();
+            Console.WriteLine("******* Thanks for your vote *******");
+            Console.WriteLine();
+            Console.WriteLine(msg);
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
